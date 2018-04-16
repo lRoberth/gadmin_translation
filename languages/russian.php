b3a721166cd0f3b842b15bde3ce58b237076a763
lRoberth <roberthdelcampo@hotmail.com>
2018-04-15 22:23:06 -0500

    Update russian.php

--- languages/russian.php
+++ languages/russian.php
@@ -139,7 +139,7 @@ $lang['applicant_id'] = "Номер заявки:";
  // DATE FORMAT, RESULT WOULD BE: 01/12/2018. PLEASE DO NOT CHANGE THE WORDS.
 // YOU CAN DO FOR EXAMPLE, year-month-day AND RESULT WILL BE: 2018-12-01
 // НЕ ИЗМЕНЯЙТЕ 'month', 'day', 'year'
-$lang['date'] = "month/day/year";
+$lang['datew'] = "month/day/year";
 // HOUR FORMAT, RESULT WOULD BE: 02:25 p.m. PLEASE DO NOT CHANGE THE WORDS
 // YOU CAN DO FOR EXAMPLE, minutes:hours meridiem AND RESULT WILL BE: 25:02 p.m
 // НЕ ИЗМЕНЯЙТЕ 'hours', 'minutes', 'meridiem'
