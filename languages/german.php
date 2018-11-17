5f4755a1b19c0143674732bb207418d9bc75f6b3
lRoberth <roberthdelcampo@hotmail.com>
2018-11-17 14:56:56 -0600

    Create german.php

---
+++ languages/german.php
@@ -0,0 +1,160 @@
+<?php
+// THIS USES THE BASIC PHP VARIABLE SYNTAX. PLEASE DON'T WRITE ANYTHING OUTSIDE THE "". IF YOU'RE GOING TO USE THE " CHAR INSIDE A STRING,
+// BE SURE TO PUT A \ CHAR BEFORE THE ", FOR EXAMPLE: Press the \"Submit\" button.
+// THESE WILL BREAK THE SYSTEM:
+// Press the "Submit" button.
+// Press the "\Submit"\ button.
+// Press the \"Submit" button.
+// Press the "Submit\" button.
+// THE RIGHT WAY IS ONLY THIS ONE:
+// Press the \"Submit\" button.
+// \" This \" Will Not \" Break \"\" Anything \"\"\"
+// HOPE YOU UNDERSTAND IT.
+// YOU CAN ACTUALLY SEE A LIVE EXAMPLE AT LINE 16.
+// THANK YOU FOR HELPING ME TRANSLATING THIS SCRIPT TO OTHER LANGUAGES.
+$lang['quest_desc'] = "Sie können hier Ihre Anwendungsfragen ändern. Denken Sie daran, ganz unten auf \"Bestätigen \" zu drücken.";
+$lang['email_invalid'] = "Die angegebene email adresse wurde nicht gefunden!";
+$lang['not_image'] = "Datei ist kein Bild";
+$lang['img_large'] = "Diese Bild Datai ist zu groß";
+$lang['types_allowed'] = "Entschuldigung, nur JPG, JPEG, PNG & GIF sind erlaubt";
+$lang['file_not_uploaded'] = "Entschuldigung, Ihre datei wurde nicht hochgeladen.";
+$lang['problem_uploading'] = "Entschuldigung, bei dem hochladen der datei ist ein fehler aufgetreten.";
+$lang['staff_applications'] = "Mitarbeiter Anwendungen";
+$lang['button_login'] = "Klicken sie auf den Button um sich anzumelden!";
+$lang['script'] = "Anwendung";
+$lang['exploiting'] = "Ausnutzungs Versuch";
+$lang['pending'] = "Ausstehend...";
+$lang['accepted'] = "Akzeptiert...";
+$lang['already_admin'] = "Sie sind bereits Administrator";
+$lang['report_to'] = "MELDEN SIE DAS AN";
+$lang['denied'] = "Verweigert...";
+$lang['apply_now'] = "Jetzt bewerben!";
+$lang['apply'] = "Sich bewerben";
+$lang['playtime'] = "Screenshot Ihrer gesamten Spielstunden:";
+$lang['warnsp'] = "Screenshot von Ihren Warnungen (type !warns In-Game):";
+$lang['email'] = "Email:";
+$lang['specify_email'] = "Bitte geben Sie Ihre E-Mail Adresse an";
+$lang['submit'] = "Einreichen";
+$lang['cancel'] = "Abbrechen";
+$lang['submit_applies'] = "Eingereichter Antrag";
+$lang['weird_stuff_report'] = "Etwas Seltsames passiert, bitte melde dies an ";
+$lang['error'] = "Fehler";
+$lang['not_found'] = "Entschuldigung, Seite wurde nicht gefunden";
+$lang['if_not'] = "Wenn Sie nicht innerhalb von 5 Sekunden umgeleitet werden, klicken Sie bitte auf";
+$lang['here'] = "hier";
+$lang['admin_p'] = "Administrationsmenü";
+$lang['change_questions'] = "Fragen ändern";
+$lang['quest'] = "Frage";
+$lang['manage_admins'] = "Administratoren verwalten";
+$lang['manage_admins_desc'] = "Beachten Sie, dass jeder hier angegebene Administrator Anwendungen, Administratoren und Bans verwalten darf.";
+$lang['steamid64'] = "SteamID64";
+$lang['specify_the'] = "Bitte spezifizieren Sie die";
+$lang['of_target'] = "von dem Ziel:";
+$lang['manage_bans'] = "Verwalte Bans";
+$lang['manage_bans_desc_1'] = "Hier können sie veralten, wer sich bewerben darf oder nicht.";
+$lang['manage_bans_desc_2'] = "Hier können sie außerdem spieler entbannen welche versucht haben das system zu hacken.";
+$lang['id'] = "ID";
+$lang['name'] = "Name";
+$lang['datew'] = "Datum";
+$lang['banned_b'] = "Banned von";
+$lang['cause'] = "Grund";
+$lang['ban_reason'] = "Geben sie den Ban Grund an: (Wenn du jemand enbannen willst dann lasse diesen Bereich einfach leer.)";
+$lang['settings'] = "Einstellungen";
+$lang['settings_desc_1'] = "Hier können Sie Ihre Systemeinstellungen in der Vorschau anzeigen oder ändern.";
+$lang['settings_desc_2'] = "Nur der Lizenznehmer hat Zugriff auf diese Einstellungen, sodass Sie sich keine Sorgen darüber machen müssen, dass andere Administratoren Ihre Kennwörter sehen.";
+$lang['general_conf'] = "Allgemeine Konfiguration";
+$lang['s_name'] = "Server Name:";
+$lang['s_name_desc'] = "Ihr server name.";
+$lang['steam_api_get'] = "Steam API (get it";
+$lang['api_desc'] = "Ihre Steam API.";
+$lang['index_url'] = "Index URL:";
+$lang['index_url_desc'] = "Fügen Sie http: // am Anfang und / am Ende ein.";
+$lang['animated_index'] = "Home animierte Nachrichten: (Zum Deaktivieren geben Sie DISABLE oder zum Verwenden von Standardtext DEFAULT ein.)";
+$lang['animated_index_desc'] = "Beispiel: Willkommen! Hast du das Zeug zum Administrator? Bewerben Sie sich jetzt für den Administrator!";
+$lang['mysql_conf'] = "MySQL Konfig";
+$lang['host'] = "Host:";
+$lang['host_desc'] = "Dein MySQL host.";
+$lang['user'] = "Nutzer:";
+$lang['user_desc'] = "Dein MySQL user.";
+$lang['pass'] = "Passwort:";
+$lang['pass_desc'] = "Dein MySQL Passwort.";
+$lang['database'] = "Datenbank:";
+$lang['database_desc'] = "Deine MySQL datenbank.";
+$lang['adv_conf'] = "Erweiterte Konfig";
+$lang['dev_access'] = "Entwickler zulassen (empfohlen).";
+$lang['yes'] = "Ja";
+$lang['no'] = "Nein";
+$lang['captcha_sys'] = "Aktivieren Sie das Captcha-System:";
+$lang['recaptcha'] = "Re-Captcha site key - You can get it"; //???
+$lang['recaptcha_desc'] = "Dein Re-Captcha seitenschlüssel.";
+$lang['recaptcha_s'] = "Re-Captcha secret key - You can get it";
+$lang['recaptcha_s_desc'] = "Your Re-Captcha secret key.";
+$lang['error_occurred_mysql'] = "In Ihrer MySQL-Konfiguration ist ein Fehler aufgetreten. Sie werden in wenigen Sekunden umgeleitet.";
+$lang['root_admin'] = "Sie können den Root-Administrator nicht löschen";
+$lang['p_hours'] = "Spielerstunden";
+$lang['warns'] = "Warnungen";
+$lang['accept'] = "Akzeptieren";
+$lang['deny'] = "Verweigern";
+$lang['remove'] = "Löschen";
+$lang['conn_error'] = "Beim Herstellen der Verbindung mit der Datenbank ist ein Fehler aufgetreten. Überprüfen Sie die Konfiguration. Wenn der Fehler weiterhin besteht, öffnen Sie ein Ticket. ERROR:";
+$lang['ban_by'] = "Sie wurden dauerhaft gebannt von ";
+$lang['ban_from'] = "von der Anwendung";
+$lang['ban_reason_sys'] = "Grund:";
+$lang['the_system'] = "das System";
+$lang['created_with'] = "Erstellt mit";
+$lang['by'] = "von";
+$lang['admin_elements'] = "Admin-Elemente";
+$lang['apps'] = "Bewerbungen";
+$lang['home'] = "Startseite";
+$lang['logout'] = "Ausloggen";
+$lang['langt'] = "Sprache";
+$lang['background_index'] = "Home-Hintergrund-URL (stellen Sie sicher, dass es sich um eine direkte Bilddatei handelt, die maximale Auflösung beträgt 1920 x 1080; geben Sie DEFAULT ein, um den Standard festzulegen):";
+$lang['background_index_desc'] = "Legen Sie die URL als Hintergrund auf der Startseite fest.";
+$lang['pic1'] = "Hintergrund der Fragen ändern (stellen Sie sicher, dass es sich um eine direkte Bilddatei handelt, die maximale Auflösung beträgt 1920 x 1080; geben Sie DEFAULT ein, um den Standard festzulegen):";
+$lang['pic1_desc'] = "Ändert die Hintergrundoptionen im Admin-Panel.";
+$lang['pic2'] = "Admins Management-Hintergrund (stellen Sie sicher, dass es sich um eine direkte Bilddatei handelt, die maximale Auflösung beträgt 1920 x 1080; geben Sie DEFAULT ein, um den Standard festzulegen):";
+$lang['pic2_desc'] = "Ändert die Hintergrundoptionen im Admin-Panel.";
+$lang['pic3'] = "Hintergrund der Bans-Verwaltung (stellen Sie sicher, dass es sich um eine direkte Bilddatei handelt, die maximale Auflösung beträgt 1920 x 1080; geben Sie DEFAULT ein, um den Standard festzulegen):";
+$lang['pic3_desc'] =  "Ändert die Hintergrundoptionen im Admin-Panel.";
+$lang['pic4'] = "Allgemeiner Konfigurationshintergrund (stellen Sie sicher, dass es sich um eine direkte Bilddatei handelt, die maximale Auflösung beträgt 1920 x 1080; geben Sie DEFAULT ein, um den Standard festzulegen):";
+$lang['pic4_desc'] = "Ändert die Hintergrundoptionen im Admin-Panel.";
+$lang['pic5'] = "Hintergrund der Serververwaltung (stellen Sie sicher, dass es sich um eine direkte Image-Datei handelt, die maximale Auflösung beträgt 1920 x 1080; geben Sie DEFAULT ein, um den Standard festzulegen):";
+$lang['pic5_desc'] = "Ändert die Hintergrundoptionen im Admin-Panel.";
+$lang['warnsys'] = "Aktiviere warnungen screenshot system:";
+$lang['timesys'] = "Aktiviere spielzeit screenshot system:";
+$lang['watch_application'] = "Review";
+$lang['comments'] = "Kommentare";
+$lang['comment_conf'] = "Kommentar System Konfig";
+$lang['comments_enable'] = "Kommentarsystem aktivieren?";
+$lang['comments_remove_root'] = "Dürfen NUR Root-Admins Kommentare entfernen?";
+$lang['comments_remove_self'] = "Sollten Admins / Root-Admins ihre eigenen Kommentare entfernen dürfen?";
+$lang['comments_admin_remove_root'] = "Sollten Admins berechtigt sein, Root-Admin-Kommentare zu entfernen?";
+$lang['comments_admin_remove_admins'] = "Sollten Administratoren andere Admin-Kommentare entfernen dürfen?";
+$lang['comments_user_see'] = "Sollten Bewerber sehen, welche Admins zu seinem Antrag Stellung nehmen?";
+$lang['comments_user_comment'] = "Sollten Bewerber in der Lage sein, sich zu ihrem Antrag zu äußern?";
+$lang['comments_user_remove'] = "Sollten Bewerber ihre eigenen Kommentare entfernen dürfen?";
+$lang['no_comments'] = "Es gibt derzeit keine Kommentare, fügen Sie jetzt einen Kommentar hinzu.";
+$lang['send_comment'] = "Kommentar senden";
+$lang['add_comment'] = "Kommentar hinzufügen";
+$lang['applicant'] = "Bewerber";
+$lang['date_at_hour'] = "um"; // RESULT WOULD BE FOR EXAMPLE: 04/14/2018 at 9:03 p.m
+$lang['applicant_id'] = "Bestätige ID";
+ // DATE FORMAT, RESULT WOULD BE: 01/12/2018. PLEASE DO NOT CHANGE THE WORDS.
+// YOU CAN DO FOR EXAMPLE, year-month-day AND RESULT WILL BE: 2018-12-01
+// JUST DON'T CHANGE/EDIT 'month', 'day', 'year'
+$lang['date'] = "day/month/year";
+// HOUR FORMAT, RESULT WOULD BE: 02:25 p.m. PLEASE DO NOT CHANGE THE WORDS
+// YOU CAN DO FOR EXAMPLE, minutes:hours meridiem AND RESULT WILL BE: 25:02 p.m
+// JUST DON'T CHANGE/EDIT 'hours', 'minutes', 'meridiem'
+$lang['hour'] = "hours:minutes meridiem";
+$lang['server_choose'] = "Für welchen Server möchten sie sich bewerben?";
+$lang['applicant_forserver'] = "Bewerbe für Server:";
+$lang['applicant_forserver_unknown'] = "Unbekannt (ID %id)";
+$lang['servers_conf'] = "Server Verwaltung";
+$lang['servers_conf_desc_1'] = "Hier können Sie Server vom System hinzufügen oder entfernen.";
+$lang['servers_conf_desc_2'] = "Um einen neuen Server hinzuzufügen, geben Sie die einzigartig Server-ID und den Servernamen ein und drücken Sie dann die grüne Taste.";
+$lang['servers_conf_desc_3'] = "Um einen Server zu entfernen, geben Sie die einzigartig Server-ID an und drücken Sie die grüne Taste.";
+$lang['server_specify_id'] = "Bitte geben Sie die einzigartig Server-ID an: (Achten Sie darauf, dass sie einzigartig ist, sonst wird sie nicht hinzugefügt)";
+$lang['server_specify_name'] = "Bitte geben Sie den Servernamen an: (Wenn Sie einen Server entfernen, lassen Sie dieses Feld leer)";
+$lang['server_not_available'] = "Der Server-Admin hat die verfügbaren Server noch nicht konfiguriert!<br>Bitte komm später wieder.";
+?>
