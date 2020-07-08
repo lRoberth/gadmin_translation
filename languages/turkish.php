617f505e409251a3e729d70ae8d968068e0016e3
lRoberth <roberthdelcampo@hotmail.com>
2020-07-08 18:39:32 -0500

    Create turkish.php

---
+++ languages/turkish.php
@@ -0,0 +1,224 @@
+<?php
+// THIS USES THE BASIC PHP VARIABLE SYNTAX. PLEASE DON'T WRITE ANYTHING OUTSIDE THE "". IF YOU'RE GOING TO USE THE " CHAR INSIDE A STRING,
+// BE SURE TO PUT A \ CHAR BEFORE THE ", FOR EXAMPLE: Press the \"Submit\" button.
+// THESE WILL BREAK THE SYSTEM:
+// Press the "Submit" button.
+// Press the "\Submit"\ button.
+// Press the \"Submit" button.
+// Press the "Submit\" button.
+// THE RIGHT WAY IS ONLY THIS ONE:
+// Press the \"Submit\" button.oh 
+// \" This \" Will Not \" Break \"\" Anything \"\"\"
+// HOPE YOU UNDERSTAND IT.
+// YOU CAN ACTUALLY SEE A LIVE EXAMPLE AT LINE 16.
+// THANK YOU FOR HELPING ME TRANSLATING THIS SCRIPT TO OTHER LANGUAGES.
+
+$lang['quest_desc'] = "Burada uygulama sorularınızı değiştirebilirsiniz, en altta \"Submit\" tuşuna basmayı unutmayınız.";
+$lang['email_invalid'] = "Girdiğiniz e-posta adresi geçerli değil";
+$lang['not_image'] = "Dosya bir resim değil";
+$lang['img_large'] = "Resim dosyası çok büyük";
+$lang['types_allowed'] = "Yalnızca 5 MB'ı aşmayan JPG, JPEG, PNG ve GIF dosyalarına izin verilir.";
+$lang['file_not_uploaded'] = "Maalesef, dosyanız yüklenmedi.";
+$lang['problem_uploading'] = "Maalesef, dosyanızı yüklerken bir sorun yaşandı.";
+$lang['staff_applications'] = "Yetkili Başvuruları";
+$lang['button_login'] = "Giriş yapmak için aşağıdaki düğmeyi tıklayın!";
+$lang['script'] = "Eklenti";
+$lang['exploiting'] = "Exploit girişimi";
+$lang['pending'] = "Bekleniyor...";
+$lang['accepted'] = "Kabul edildi...";
+$lang['already_admin'] = "Zaten yetkilisin";
+$lang['report_to'] = "ŞU KİŞİYE RAPOR ET";
+$lang['denied'] = "Reddedildi...";
+$lang['apply_now'] = "Şimdi başvur!";
+$lang['apply'] = "Başvur";
+$lang['playtime'] = "Toplam Oynama saatinizin ekran görüntüsü:";
+$lang['warnsp'] = "Uyarılarınızın ekran görüntüsü (Oyun içi !warns yazınız)";
+$lang['email'] = "E-Posta:";
+$lang['specify_email'] = "Lütfen e-postanızı belirtiniz";
+$lang['submit'] = "Sun";
+$lang['cancel'] = "İptal et";
+$lang['submit_applies'] = "Gönderilen Başvurular";
+$lang['weird_stuff_report'] = "Tuhaf bir şeyler oluyor, lütfen bunu yetkiliye bildiriniz";
+$lang['error'] = "Hata";
+$lang['not_found'] = "Üzgünüz, sayfa bulunamadı";
+$lang['if_not'] = "5 saniye içinde yönlendirilmezseniz, burayı tıklayınız";
+$lang['here'] = "buraya";
+$lang['admin_p'] = "Yetkili Paneli";
+$lang['change_questions'] = "Soruları değiştir";
+$lang['quest'] = "Soru";
+$lang['manage_admins'] = "Yetkilileri Yönet";
+$lang['manage_admins_desc'] = "Burada belirtilen tüm yetkililerin başvuruları, yetkileri ve yasakları yönetmesine izin verileceğini unutmayınız.";
+$lang['steamid64'] = "SteamID64";
+$lang['specify_the'] = "Sebepini belirtiniz";
+$lang['of_target'] = "hedefin:";
+$lang['manage_bans'] = "Yasakları Yönet";
+$lang['manage_bans_desc_1'] = "Burada kimlerin başvurması gerekip gerekmediğini kontrol edebilirsiniz.";
+$lang['manage_bans_desc_2'] = "Burada ayrıca sistemi sömürmeye çalışan insanları da yasaklayabilirsiniz.";
+$lang['id'] = "ID";
+$lang['name'] = "İsim";
+$lang['datew'] = "Tarih";
+$lang['banned_b'] = "Yasaklayan";
+$lang['cause'] = "Sebep";
+$lang['ban_reason'] = "Yasaklama nedenini belirtin: (birinin yasağını kaldırıyorsanız, bunu boş bırakın)";
+$lang['settings'] = "Ayarlar";
+$lang['settings_desc_1'] = "Burada sistem ayarlarınızı önizleyebilir veya değiştirebilirsiniz.";
+$lang['settings_desc_2'] = "Bu ayarlara yalnızca lisans sahibi erişebilir, bu nedenle şifrelerinizi arayan başka bir yetkililer için endişelenmenize gerek yoktur.";
+$lang['general_conf'] = "Genel yapılandırma";
+$lang['s_name'] = "Sunucu ismi:";
+$lang['s_name_desc'] = "Sunucu adınız.";
+$lang['steam_api_get'] = "Steam API";
+$lang['api_desc'] = "Senin Steam API";
+$lang['index_url'] = "Dizin URL'si:";
+$lang['index_url_desc'] = "Başına http:// ekleyin ve sonunda slash ekleyin.";
+$lang['animated_index'] = "Ev animasyonlu mesajları: (devre dışı bırakmak için DISABLE veya varsayılan metni kullanmak için DEFAULT yazın.)";
+$lang['animated_index_desc'] = "Örnek: Hoşgeldiniz!, Yetkili olmak ister misiniz?, Yetkililik için şimdi başvurun!";
+$lang['mysql_conf'] = "MySQL Yapılandırması";
+$lang['host'] = "Host:";
+$lang['host_desc'] = "MySQL sunucunuz.";
+$lang['user'] = "Oyuncu:";
+$lang['user_desc'] = "MySQL kullanıcınız.";
+$lang['pass'] = "Şifre:";
+$lang['pass_desc'] = "MySQL şifreniz.";
+$lang['database'] = "Veri tabanı:";
+$lang['database_desc'] = "MySQL veritabanınız.";
+$lang['adv_conf'] = "Gelişmiş Yapılandırma";
+$lang['dev_access'] = "Geliştiricilere izin ver (önerilir).";
+$lang['yes'] = "Evet";
+$lang['no'] = "Hayır";
+$lang['captcha_sys'] = "Captcha Sistemini Etkinleştir:";
+$lang['recaptcha'] = "Yeniden Captcha site anahtarı - alabilirsiniz";
+$lang['recaptcha_desc'] = "Re-captcha site anahtarınız.";
+$lang['recaptcha_s'] = "Re-Captcha gizli anahtarınız - alabilirsiniz";
+$lang['recaptcha_s_desc'] = "Re-Captcha gizli anahtarınız.";
+$lang['error_occurred_mysql'] = "MySQL yapılandırmanızda bir hata oluştu, birkaç saniye içinde yönlendirileceksiniz.";
+$lang['root_admin'] = "Ana Yetkiyi silemezsiniz";
+$lang['p_hours'] = "Oyuncu Saati";
+$lang['warns'] = "Uyarıları";
+$lang['accept'] = "Kabul et"; 
+$lang['deny'] = "Reddet";
+$lang['remove'] = "Kaldır";
+$lang['conn_error'] = "Veritabanına bağlanırken bir sorun oluştu, büyük olasılıkla MySQL kullanıcı adınız mevcut değil, şifre yanlış, veritabanı mevcut değil veya ana makinenizle bağlantı kuramıyoruz. <br> Hata:";
+$lang['ban_by'] = "Tarafından kalıcı olarak yasaklandınız";
+$lang['ban_from'] = "başvurmaktan";
+$lang['ban_reason_sys'] = "Sebep:";
+$lang['the_system'] = "sistem";
+$lang['created_with'] = "İle oluşturuldu";
+$lang['by'] = "tarafından";
+$lang['admin_elements'] = "Yetkili Öğeleri";
+$lang['apps'] = "Başvurular";
+$lang['home'] = "Ev";
+$lang['logout'] = "Çıkış yap";
+$lang['langt'] = "Dil";
+$lang['background_index'] = "Ana arka plan URL'si (doğrudan resim dosyası olduğundan emin olun, maksimum çözünürlük 1920x1080'dir, varsayılan olarak ayarlamak için DEFAULT yazınız):";
+$lang['background_index_desc'] = "URL'yi ana sayfada arka plan olarak ayarlayın.";
+$lang['pic1'] = "Sorular arka planını değiştirme (doğrudan bir resim dosyası olduğundan emin olun, maksimum çözünürlük 1920x1080'dir, varsayılan olarak ayarlamak için DEFAULT yazın):";
+$lang['pic1_desc'] = "Yetkili panelindeki arka plan seçeneklerini değiştirir.";
+$lang['pic2'] = "Sunucu Yönetimi arka planı (doğrudan bir resim dosyası olduğundan emin olun, maksimum çözünürlük 1920x1080'dir, varsayılan olarak ayarlamak için DEFAULT yazın):";
+$lang['pic2_desc'] = "Yetkili panelindeki arka plan seçeneklerini değiştirir.";
+$lang['pic3'] = "Yasaklar Yönetimi arka plan(doğrudan bir resim dosyası olduğundan emin olun, maksimum çözünürlük 1920x1080'dir, varsayılan olarak ayarlamak için DEFAULT yazın):";
+$lang['pic3_desc'] =  "Yetkili panelindeki arka plan seçeneklerini değiştirir.";
+$lang['pic4'] = "Genel Yapılandırma arka planı (doğrudan bir resim dosyası olduğundan emin olun, maksimum çözünürlük 1920x1080'dir, varsayılan olarak ayarlamak için DEFAULT yazın):";
+$lang['pic4_desc'] = "Yetkili panelindeki arka plan seçeneklerini değiştirir.";
+$lang['pic5'] = "Sunucu Yönetimi arka planı (doğrudan bir resim dosyası olduğundan emin olun, maksimum çözünürlük 1920x1080'dir, varsayılan olarak ayarlamak için DEFAULT yazın):";
+$lang['pic5_desc'] = "Yetkili panelindeki arka plan seçeneklerini değiştirir.";
+$lang['warnsys'] = "Etkinleştir ekran görüntüsü sistemini devre dışı bırakır:";
+$lang['timesys'] = "Oynatma süresi ekran görüntüsü sistemini etkinleştirin:";
+$lang['watch_application'] = "Gözden geçir";
+$lang['comments'] = "Yorumlar";
+$lang['comment_conf'] = "Yorum ayarları";
+$lang['comments_enable'] = "Yorum yapma sistemini etkinleştir?";
+$lang['comments_remove_root'] = "SADECE ana yetkililerin yorumları kaldırmasına izin verilmeli mi?";
+$lang['comments_remove_self'] = "Yetkililerin / Ana Yetkililerin kendi yorumlarını kaldırmalarına izin verilmeli mi?";
+$lang['comments_admin_remove_root'] = "Yetkililerin ana yetkililerin yorumlarını kaldırmasına izin verilmeli mi?";
+$lang['comments_admin_remove_admins'] = "Yetkililerin diğer yetkilinin yorumlarını kaldırmasına izin verilmeli mi?";
+$lang['comments_user_see'] = "Başvuru sahipleri hangi yetkililerin başvuruları hakkında yorum yaptığını görmeli mi?";
+$lang['comments_user_comment'] = "Başvuru sahipleri başvuruları hakkında yorum yapabilmeli mi?";
+$lang['comments_user_remove'] = "Başvuru sahiplerinin kendi yorumlarını kaldırmalarına izin verilmeli mi?";
+$lang['no_comments'] = "Şu anda yorum yok, aşağıya yorum ekleyin.";
+$lang['send_comment'] = "Yorum gönder";
+$lang['add_comment'] = "Yorum ekle";
+$lang['applicant'] = "Başvuran";
+$lang['date_at_hour'] = "da"; // RESULT WOULD BE FOR EXAMPLE: 04/14/2018 at 9:03 p.m
+$lang['applicant_id'] = "ID uygula";
+ // DATE FORMAT, RESULT WOULD BE: 01/12/2018. PLEASE DO NOT CHANGE THE WORDS.
+// YOU CAN DO FOR EXAMPLE, year-month-day AND RESULT WILL BE: 2018-12-01
+// JUST DON'T CHANGE/EDIT 'month', 'day', 'year'
+$lang['date'] = "ay/gün/yıl";
+// HOUR FORMAT, RESULT WOULD BE: 02:25 p.m. PLEASE DO NOT CHANGE THE WORDS
+// YOU CAN DO FOR EXAMPLE, minutes:hours meridiem AND RESULT WILL BE: 25:02 p.m
+// JUST DON'T CHANGE/EDIT 'hours', 'minutes', 'meridiem'
+$lang['hour'] = "saat:dakika meridiem";
+$lang['server_choose'] = "Hangi sunucuya başvuruyorsunuz?";
+$lang['applicant_forserver'] = "Sunucu için başvuru:";
+$lang['applicant_forserver_unknown'] = "Bilinmeyen (ID %id)";
+$lang['servers_conf'] = "Sunucuları Yönet";
+$lang['servers_conf_desc_1'] = "Burada sunucuları sisteme ekleyebilir veya sistemden kaldırabilirsiniz.";
+$lang['servers_conf_desc_2'] = "Yeni bir sunucu eklemek için, sunucunun benzersiz kimliğini ve sunucu adını belirtin, ardından yeşil düğmeye basın.";
+$lang['servers_conf_desc_3'] = "Bir sunucuyu kaldırmak için, sunucunun benzersiz kimliğini belirtin ve yeşil düğmeye basın.";
+$lang['server_specify_id'] = "Lütfen benzersiz sunucu kimliğini belirtin: (benzersiz olduğundan emin olun, aksi takdirde eklenmez)";
+$lang['server_specify_name'] = "Lütfen sunucu adını belirtin: (bir sunucuyu kaldırıyorsanız, bunu boş bırakın)";
+$lang['server_not_available'] = "Sunucu yetkilisi henüz kullanılabilir sunucuları ayarlamadı! <br> Lütfen daha sonra tekrar gelin.";
+
+
+$lang['ranks'] = "Yetkiler";
+$lang['applicant_forrank'] = "Yetki Başvurusu";
+$lang['server_specify_rank'] = "Lütfen sunucu tarafından kullanılabilen Yetkileri virgülle ayırarak belirtin. Örnek: tmod, mod, admin, Head-Admin";
+$lang['servers_conf_desc_4'] = "Bir sunucuyu düzenlemek için, sunucunun benzersiz kimliğini belirtin, yeni veriler yazın ve yeşil düğmeye basın.";
+$lang['admin_removed_success'] = "Yetkili başarıyla kaldırıldı.";
+$lang['server_sucess_removed'] = "Sunucu başarıyla kaldırıldı.";
+$lang['server_sucess_edited'] = "Sunucu başarıyla düzenlendi.";
+$lang['server_sucess_added'] = "Sunucu başarıyla eklendi.";
+$lang['server_error_rank'] = "Lütfen virgülle ayrılmış en az bir Yetki belirtin! <br> Örnek: tmod, moderatör, admin";
+$lang['error_msg_title'] = "Bir saniye!";
+$lang['success_msg_title'] = "Başarılı!";
+$lang['rank_choose'] = "Hangi yetkiye başvuruyorsunuz?";
+$lang['please_server_select'] = "Lütfen bir sunucu seçin";
+$lang['please_server_select2'] = "Lütfen yukarıda bir sunucu seçin";
+$lang['rank_set'] = "Ayarlanacak Yetki";
+
+$lang['app_wait_title'] = "Bir süre bekleyin lütfen";
+$lang['app_wait_desc'] = "Şu anda başvurunuzu gönderiyoruz.";
+
+$lang['app_result:ok'] = "Başvurunuz başarıyla gönderildi.";
+$lang['app_result:ok_title'] = "Başarılı!";
+
+$lang['app_result:error'] = "Bilinmeyen bir hata oluştu! Lütfen daha sonra tekrar deneyiniz.";
+$lang['app_result:error_title'] = "Olamaz!";
+
+$lang['app_result:invalidserver'] = "Lütfen devam etmeden önce en az bir sunucu seçin!";
+$lang['app_result:invalidserver_title'] = "Bir saniye!";
+
+$lang['app_result:uploaderror'] = "Resimlerinizi karşıya yüklemeye çalışırken bir hata oluştu. Lütfen başka bir tarayıcı kullanmayı deneyin.";
+$lang['app_result:uploaderror_title'] = "Olamaz!";
+
+$lang['app_result:upload_compat'] = "Yalnızca 5 MB'ı aşmayan JPG, JPEG, PNG ve GIF dosyalarına izin verilir.";
+$lang['app_result:upload_compat_title'] = "Bir saniye!";
+
+$lang['app_result:appbanned'] = "Şu anda yeni başvuru göndermeniz yasaklandı.";
+$lang['app_result:appbanned_title'] = "Bir dakika!";
+
+$lang['app_result:captcha'] = "Captcha'yı geçmedin! Lütfen tekrar deneyiniz.";
+$lang['app_result:captcha_title'] = "Bir saniye!";
+
+$lang['app_result:email'] = "Lütfen geçerli eposta adresini giriniz.";
+$lang['app_result:email_title'] = "Bir saniye!";
+
+$lang['app_result:applied'] = "Sistemde zaten kayıtlı bir başvurunuz var!";
+$lang['app_result:applied_title'] = "Bir saniye!";
+
+$lang['delete_ov'] = "Zaman içinde başvurular silinsin mi? (yeniden başvurmaya izin vermek için)";
+$lang['delete_del'] = "Başvuru reddedilir veya kabul edilirse başvurunun kaldırılması için kaç gün geçmesi gerekir?";
+
+$lang['backup'] = "Veritabanı yedekleme";
+$lang['backup_config'] = "Yedekleme Yapılandırması";
+
+$lang['mandatory'] = "Zorunlu";
+$lang['mandatory_2'] = "Bu cevap zorunluydu ancak başvuran cevap vermedi";
+
+$lang['gmod_playtime'] = "Oynama süresi:";
+$lang['hours'] = "saat";
+
+$lang['deny_description'] = "İnkarın nedenleri nelerdir? Bu mesaj başvuru sahibine gösterilecektir.";
+
+$lang['app_pending'] = 'Bekleniyor';
+?>
